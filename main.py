945013999504b476f3ed52693524db6bfad18fb0
Notreve46 <167482177+Notreve46@users.noreply.github.com>
2024-05-16 22:31:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+Bimestre1 = float(input("Escreva sua nota do Bimestre1:  "))
+Bimestre2 = float(input("Escreva sua nota do Bimestre2:  "))
+Bimestre3 = float(input("Escreva sua nota do Bimestre1:  "))
+Bimestre4 = float(input("Escreva sua nota do Bimestre1:  "))
+
+media = (Bimestre1+Bimestre2+Bimestre3+Bimestre4)/4
+
+print(f"sua média é{media}")
+
